e0d9244fc65816ddbf49fa5364df2b531cef0ba2
abhijnyaks <abhijnyaks11@gmail.com>
2026-07-29 11:45:48 +0530

    day 12 react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
